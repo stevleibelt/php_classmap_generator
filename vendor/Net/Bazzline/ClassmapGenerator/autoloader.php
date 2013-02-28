66cdb57a7536a88f4d3effa809411c3321056245
stevleibelt <artodeto@arcor.de>
2013-02-28 23:26:31 +0100

    Added classmap autoloading.

--- vendor/Net/Bazzline/ClassmapGenerator/autoloader.php
+++ vendor/Net/Bazzline/ClassmapGenerator/autoloader.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * @todo Add classmap to speed up autoloading
  * @author stev leibelt
  * @param string $className
  * @since 2013-02-27
@@ -16,11 +15,29 @@ function autoloadFromFilesystem($className)
         $filePath = $includePath . DIRECTORY_SEPARATOR . $fileName;
 
         if (file_exists($filePath)) {
-            require_once $filePath;
+            require $filePath;
 
             break;
         }
     }
 }
 
+/**
+ * @author stev leibelt
+ * @param string $className
+ * @since 2013-02-28
+ */
+function autoloadFromFilesystemWithClassmap($classname)
+{
+    $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $classname) . '.php';
+    $classnameToFilepath = require 'autoloader_classmap.php';
+
+    if (isset($classnameToFilepath[$classname])) {
+        require $classnameToFilepath[$classname];
+    } else {
+        return false;
+    }
+}
+
+spl_autoload_register('autoloadFromFilesystemWithClassmap');
 spl_autoload_register('autoloadFromFilesystem');
