bab81e687bff69c2454e1586248986f5518f5822
stevleibelt <artodeto@arcor.de>
2013-03-05 22:42:32 +0100

    Prepared for unittests.

---
+++ tests/vendor/Net/Bazzline/ClassmapGenerator/Application/ApplicationTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author stev leibelt
+ * @since 2013-03-05
+ */
+class ApplicationTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @author stev leibelt
+     * @since 2013-03-05
+     */
+    public function testInterface()
+    {
+        $this->assertTrue(true);
+    }
+}
