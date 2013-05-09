9c1f68db90f984a4667581bcf5bdbf94367271eb
stevleibelt <stev@leibelt.de>
2013-05-10 00:08:06 +0200

    Fixed autoloading error

--- bin/net_bazzline_classmap_generator.php
+++ bin/net_bazzline_classmap_generator.php
@@ -7,12 +7,13 @@
 
 chdir(realpath(getcwd()));
 
+require 'vendor/autoload.php';
+
 //autloader for development
 if (file_exists('src/Net/Bazzline/ClassmapGenerator/basicAutoloader.php')) {
     echo 'Development mode.' . PHP_EOL;
     echo 'Loading autoloaders' . PHP_EOL;
 
-    require 'vendor/autoload.php';
     require 'src/Net/Bazzline/ClassmapGenerator/basicAutoloader.php';
 }
 
